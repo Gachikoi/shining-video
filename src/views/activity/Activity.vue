8e8305ab6a58aadea0e02bff4976bba87526e278
Gach1koi <k1ss.sakura@outlook.com>
2024-11-02 11:14:10 +0800

    1

--- src/views/activity/Activity.vue
+++ src/views/activity/Activity.vue
@@ -2,9 +2,9 @@
   <div class="flex justify-center">
     <div class="w-screen max-w-screen-xl space-y-10">
       <H1Container title="活动内容">
-        <p class="indent-8 max-w-screen-lg tracking-wider leading-loose text-justify">视频组全称Shining ACG Video
-          Studio，即晒你动漫社视频组，隶属于宣传部，经过乌鸦、渣渣、令君、梓泠、书名号、掌控-物质、ガチ恋等前辈们和组长们的发展，现在是研习ACG视频制作技术，并为社团提供数字媒体技术支持的部门。内容包括但不限于MAD、MMD、PV制作，书刊及海报设计制作，以及其他实用工具的研究应用。
-        </p>
+        <p class="indent-8 max-w-screen-lg tracking-wider leading-loose text-justify">除了线上的群聊、企划外，我们还有许多线下活动。</p>
+        <p class="indent-8 max-w-screen-lg tracking-wider leading-loose text-justify">每周六14点，我们会在社团活动室进行部门活动，包括MAD教学、视频鉴赏、</p>
+        <p class="indent-8 max-w-screen-lg tracking-wider leading-loose text-justify">除了线上的群聊、企划外，我们还有许多线下活动。</p>
       </H1Container>
       <H1Container title="活动影像">
         <ul class="columns-md space-y-5 gap-x-5 *:*:shadow-md">
@@ -15,8 +15,8 @@
       </H1Container>
       <H1Container title="?">
         <ul class="columns-xs w-full space-y-5 gap-x-5 *:*:shadow-md">
-          <li v-for="{ id, path } in chatPictures" :key="id"><img loading="lazy" style="box-shadow: 0 0 2px 2px rgba(0, 0, 0, 0.1);"
-              :src="serverURL + path" alt="">
+          <li v-for="{ id, path } in chatPictures" :key="id"><img loading="lazy"
+              style="box-shadow: 0 0 2px 2px rgba(0, 0, 0, 0.1);" :src="serverURL + path" alt="">
           </li>
         </ul>
       </H1Container>
